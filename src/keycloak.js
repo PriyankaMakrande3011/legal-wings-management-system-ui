a9ed4c45b45a794a17935c6fdee007e9face63f8
Tanmay9623 <146207054+Tanmay9623@users.noreply.github.com>
2026-01-06 19:04:00 +0530

    Update Keycloak server URL to production

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'http://localhost:8080/',       // Keycloak server URL (local)
+  url: 'https://legalwingcrm.in:8443/',             // fixed Keycloak server URL (local)
   realm: 'LEGAL_WING',                 // Your realm name
   clientId: 'legal-wing',          // Client ID for your client
 });
